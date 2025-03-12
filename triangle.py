9dcf5c1cf5ae29e85a78a82c1a21e91357c25bf0
Iris1618 <dongting.24@intl.zju.edu.cn>
2025-03-12 13:42:34 +0800

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,2 @@
+for i in range(1,11):    #use this to run 10 times
+    print(int((i+1)*i/2))  #caculate by formula
---
+++ variables.py
@@ -0,0 +1,16 @@
+a=15
+b=75
+c=a+b
+d=90
+e=5
+f=d+egi
+if c>f:
+    print("car-based commute faster")
+elif c<f:
+    print("bus-based commute faster")
+else:
+    print("equal")
+X=True
+Y=False
+W= X and Y
+print(W)
