4bc26b22fa84396083927e0a817f4ca49fa2996a
Iris1618 <dongting.24@intl.zju.edu.cn>
2025-05-14 06:54:55 +0100

    commit Practical13

---
+++ Practical13/alignment.py
@@ -0,0 +1,66 @@
+human_sod2=open("c:/Users/Iriss/Desktop/IBI/IBI1_2024-25/Practical13/human_sod2.fasta",'r')
+mouse_sod2=open("c:/Users/Iriss/Desktop/IBI/IBI1_2024-25/Practical13/mouse_sod2.fasta",'r')
+BLOSUM62=open("c:/Users/Iriss/Desktop/IBI/IBI1_2024-25/Practical13/BLOSUM62.txt",'r')
+random=open("c:/Users/Iriss/Desktop/IBI/IBI1_2024-25/Practical13/random.fasta",'r')
+
+def get_blosum_matrix(file):
+    lines=[]
+    for line in file:
+        if not line[0]=="#" and line.strip():
+            lines.append(line.strip().split())
+    header = lines[0]
+    dictionary = {}
+    for row in lines[1:]:
+        aa1 = row[0]
+        dictionary[aa1] = {}
+        for aa2, score in zip(header, row[1:]):
+            dictionary[aa1][aa2] = int(score)
+    return dictionary
+
+def get_fasta_sequence(file):
+    lines = file.readlines()
+    sequence = ''
+    for line in lines:
+        if not line[0]=='>':
+            sequence += line.strip()
+    return sequence
+
+def compare_sequences(seq1, seq2, blosum62):
+    scores = []
+    identical = 0
+    for a1, a2 in zip(seq1, seq2):
+        if a1 in blosum62 and a2 in blosum62[a1]:
+            score = blosum62[a1][a2]
+        else:
+            score = 0  
+        scores.append(score)
+        if a1 == a2:
+            identical += 1
+    total_score = sum(scores)
+    similarity_percent = (identical / len(seq1)) * 100
+    return total_score, similarity_percent, scores
+
+def print_result(name1, seq1, name2, seq2, score, identity, score_vector):
+    print(f"Comparison: {name1} vs {name2}")
+    print(f"{name1} Sequence:\n{seq1}\n")
+    print(f"{name2} Sequence:\n{seq2}\n")
+    print("Optional Alignment (position-by-position):")
+    for i in range(len(seq1)):
+        print(f"{seq1[i]}  {seq2[i]}  Score: {score_vector[i]}")
+    print("\nSummary:")
+    print(f"Total Score: {score}")
+    print(f"Similarity percent: {identity:.2f}%\n")
+
+blosum62=get_blosum_matrix(BLOSUM62)
+human_sequence=get_fasta_sequence(human_sod2)
+mouse_sod2.seek(0)
+mouse_sequence=get_fasta_sequence(mouse_sod2)
+random.seek(0)
+random_sequence=get_fasta_sequence(random)
+
+score, identity, vector = compare_sequences(human_sequence, mouse_sequence, blosum62)
+print_result('human_SOD2',human_sequence,'mouse_sequence',mouse_sequence,score,identity,vector)
+score, identity, vector = compare_sequences(human_sequence, random_sequence, blosum62)
+print_result('human_SOD2',human_sequence,'random_sequence',random_sequence,score,identity,vector)
+score, identity, vector = compare_sequences(mouse_sequence, random_sequence, blosum62)
+print_result('mouse_SOD2',mouse_sequence,'random_sequence',random_sequence,score,identity,vector)
