8370f8ddbc4accfb8b0ac85ff4a726bcb867661e
Iris1618 <dongting.24@intl.zju.edu.cn>
2025-05-27 15:38:13 +0100

    use better regular expressions(\w+)

--- Practical7/tata_genes.py
+++ Practical7/tata_genes.py
@@ -1,20 +1,22 @@
 import datetime
 import re
 start_time = datetime.datetime.now()
-input=open("c:/Users/Iriss/Desktop/IBI/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa",'r')
+input_file=open("c:/Users/Iriss/Desktop/IBI/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa",'r')
 gene_name=[]
 last_seq=[]
 include_tata=[]
 sequence=''
 tata=r'TATA(A|T)A(A|T)'
-for line in input:
+for line in input_file:
     line=line.rstrip()
     if line[0]=='>':
         if len(gene_name)!=0:
             sequence=''.join(last_seq)
             if re.search(tata, sequence):
                 include_tata.append([gene_name, sequence])
-        gene_name = re.findall(r'gene:.+?\s',line)[0][5:]
+        gene_name = re.findall(r'gene:(\w+)', line)
+        if gene_name:
+            gene_name = gene_name[0]
         last_seq = []
     else:
         last_seq.append(line)
