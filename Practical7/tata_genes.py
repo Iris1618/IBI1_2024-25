4ebd059d8705e4c61e5c46aa3a7684c311cc943b
Iris1618 <dongting.24@intl.zju.edu.cn>
2025-05-27 15:39:37 +0100

    close the input and output files

--- Practical7/tata_genes.py
+++ Practical7/tata_genes.py
@@ -25,10 +25,12 @@ if len(gene_name)!=0 :
             sequence=''.join(last_seq)
             if re.search(tata, sequence):
                 include_tata.append([gene_name, sequence])
+input_file.close()
 output=open('c:/Users/Iriss/Desktop/IBI/tata_genes.fa', 'w')
 for i in include_tata:
     output.write('>'+i[0]+'\n')
     output.write(i[1]+'\n')
+output.close()
 end_time = datetime.datetime.now()
 runtime = end_time - start_time
 print("runtime", runtime)
