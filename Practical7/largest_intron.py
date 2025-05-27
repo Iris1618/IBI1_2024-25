02392432438c347cbb31e757e9b44330d67cc2c9
Iris1618 <dongting.24@intl.zju.edu.cn>
2025-05-27 15:21:18 +0100

    Add if y: To prevent errors if list is empty

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,6 +1,9 @@
 import re
 seq='ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA' 
 y = re.findall(r'GT.*?AG', seq)
-largest = max(y, key=len)
-print("Longest intron:", largest)
-print("Length:", len(largest))
+if y:
+    largest = max(y, key=len)
+    print("Longest intron:", largest)
+    print("Length:", len(largest))
+else:
+    print("No intron found.")
