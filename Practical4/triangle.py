1d8f79533ec4e90cc4f033489e32257b2e5481cf
Iris1618 <dongting.24@intl.zju.edu.cn>
2025-05-27 11:13:02 +0100

    add a brief explanation of the formula

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,2 +1,4 @@
 for i in range(1,11):    #use this to run 10 times
     print(int((i+1)*i/2))  #caculate by formula
+# This formula works because the sum of the first n natural numbers can be visualized as a triangle.
+# For example, the first triangular number is 1 (1 point), the second is 3 (1+2 points), the 3rd is 6 (1+2+3 points), and so on.
