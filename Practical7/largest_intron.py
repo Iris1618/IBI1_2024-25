d2cdcc053cc496b578adfd7fd952b39362f51fb1
Iris1618 <dongting.24@intl.zju.edu.cn>
2025-05-27 15:19:37 +0100

    Find out all the candidate introns

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,5 +1,6 @@
 import re
 seq='ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA' 
-y= re.findall(r'GT.+AG',seq)
-print(y)
-print(len(y[0]))
+y = re.findall(r'GT.*?AG', seq)
+largest = max(y, key=len)
+print("Longest intron:", largest)
+print("Length:", len(largest))
