12bd0d628d886049fa205050e533f9c2d23ac876
Iris1618 <dongting.24@intl.zju.edu.cn>
2025-05-27 12:59:39 +0100

    add a brief comment explaining structure

--- Practical4/bmi_caculator.py
+++ Practical4/bmi_caculator.py
@@ -1,9 +1,10 @@
-w=int(input("please enter your weight _kg:"))
-h=float(input("please enter your height_m:"))
-bmi=w/(h**2)  #caculate bmi
-if bmi>30:             #compare
+w=int(input("please enter your weight _kg:")) # User input for weight in kilograms
+h=float(input("please enter your height_m:")) # User input for height in meters
+bmi=w/(h**2)  ## Calculate BMI using the formula: weight (kg) / (height (m) ^ 2)
+# Compare the calculated BMI to determine the category
+if bmi>30:           # If BMI is greater than 30  
     print("obese")
-elif bmi<18.5:
+elif bmi<18.5:       # If BMI is less than 18.5
     print("underweight")
-else:
+else:                # If BMI is between 18.5 and 30
     print("normal")
