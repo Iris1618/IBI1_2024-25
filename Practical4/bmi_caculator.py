8c6ff9dddb0ac3f8b4b2cd15e2f70f0899c266c6
Iris1618 <dongting.24@intl.zju.edu.cn>
2025-05-27 12:55:58 +0100

    correct the bmi formula

--- Practical4/bmi_caculator.py
+++ Practical4/bmi_caculator.py
@@ -1,6 +1,6 @@
 w=int(input("please enter your weight _kg:"))
 h=float(input("please enter your height_m:"))
-bmi=w/h   #caculate bmi
+bmi=w/(h**2)  #caculate bmi
 if bmi>30:             #compare
     print("obese")
 elif bmi<18.5:
