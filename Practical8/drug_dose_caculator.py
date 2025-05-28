3416144017347468f39225802b4bf6299a0f3a07
Iris1618 <dongting.24@intl.zju.edu.cn>
2025-05-28 03:01:13 +0100

    Retain two decimal places in the output and the unit

--- Practical8/drug_dose_caculator.py
+++ Practical8/drug_dose_caculator.py
@@ -6,7 +6,8 @@ def drug_dose_calculator(weight, strength_of_paracetamol):
         return "weight out of range(10-100kg)"
     if strength not in [120,250]:
         return "invalid strength"
-    return r*weight/strength*5
+    volume=r*weight/strength*5
+    return f"{volume:.2f}ml"
 weight=input("Please input the weight of children up to 18:")
 strength_of_paracetamol=input("Please input the strength of paracetamol(120mg/5ml or 250mg/5ml)--120 or 250 ")
 print(drug_dose_calculator(weight, strength_of_paracetamol))
