594a32c219166b6314a2a4691649da443f35b678
Iris1618 <dongting.24@intl.zju.edu.cn>
2025-05-28 02:50:41 +0100

    change the mis-spellings

--- Practical8/drug_dose_caculator.py
+++ Practical8/drug_dose_caculator.py
@@ -1,4 +1,4 @@
-def drug_dose_caculator(weight, strength_of_paracetamol):
+def drug_dose_calculator(weight, strength_of_paracetamol):
     r=15
     if 10<int(weight)<100:
         return r*int(weight)/int(strength_of_paracetamol)*5
@@ -7,4 +7,4 @@ def drug_dose_caculator(weight, strength_of_paracetamol):
     
 weight=input("Please input the weight of children up to 18:")
 strength_of_paracetamol=input("Please input the strength of paracetamol(120mg/5ml or 250mg/5ml)--120 or 250 ")
-print(drug_dose_caculator(weight, strength_of_paracetamol))
+print(drug_dose_calculator(weight, strength_of_paracetamol))
